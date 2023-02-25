48b8d781ed93bca1bd59d9c306fded0b4ffdb843
dancunrandy <dancunrandy@gmail.com>
2023-02-25 16:21:36 +0300

    complete assignment

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,18 @@
 class Character < ActiveRecord::Base
+    belongs_to :actor
+    belongs_to :show
+    
+    def say_that_thing_you_say
+      "#{self.name} always says: #{self.catchphrase}"
+    end
+    
+    # The following methods can be created using the `belongs_to` association macro
+    def actor
+      super
+    end
+    
+    def show
+      super
+    end
+  end
   
-end
